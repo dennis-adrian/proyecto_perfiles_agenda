eb7adf2f5f4c4c664c2c3b18b86a011162e50215
phreakphreak <wonkaphreak@gmail.com>
2020-06-25 21:32:05 -0400

    he modificado frmperfiles

--- CapaPresentacion/Control.cs
+++ CapaPresentacion/Control.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaNegocio
+namespace CapaPresentacion
 {
-    public class Class1
+    public class Control
     {
+
     }
 }
