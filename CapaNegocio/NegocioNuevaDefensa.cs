6ab9fbf447f2855279c737232c735c1415512140
phreakphreak <wonkaphreak@gmail.com>
2020-07-02 09:16:43 -0400

    clase negocionuevadefensa

--- CapaNegocio/NegocioNuevaDefensa.cs
+++ CapaNegocio/NegocioNuevaDefensa.cs
@@ -6,7 +6,73 @@ using System.Threading.Tasks;
 
 namespace CapaNegocio
 {
-    class NegocioNuevaDefensa
+    public class NegocioNuevaDefensa : ValidationInputUI
     {
+        #region  Construct
+        public NegocioNuevaDefensa(){
+
+        }
+
+        #endregion
+
+        
+        #region Instancias
+
+
+        #endregion
+        #region Atributos
+        
+        Object[] parametros;
+
+        #endregion
+        #region  Metodos
+        public void ControlInput(Object[] Collection)
+        {
+            try
+            {
+                Validation(Collection);
+                parametros = Collection;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("" + e);
+            }
+
+        }
+        public void Main()
+        {
+            try
+            {
+                NuevaDefensa(parametros);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("" + e);
+            }
+           
+
+        }
+        public void NuevaDefensa(Object[] Collection)
+        {
+            try
+            {
+                
+                
+                                                          
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("" + e);
+            }
+        }
+        #endregion
+
+
+        #region Destruct
+
+        ~NegocioNuevaDefensa(){
+
+        }
+        #endregion
     }
 }
