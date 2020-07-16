33cbf3f434831e3792aec0de9a90bcf8683f63e5
Dennis Adrian <adriangbetancur@gmail.com>
2020-07-16 10:45:11 -0400

    Cambio color en frmtesis

--- CapaPresentacion/FrmTesis.Designer.cs
+++ CapaPresentacion/FrmTesis.Designer.cs
@@ -388,6 +388,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(805, 457);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnGuardar);
