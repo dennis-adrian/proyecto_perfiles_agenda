633afa07c6b3d2fab86179a084d64d87823a3087
fernando98cd <luis12cd@gmail.com>
2021-10-21 22:11:39 -0400

    documentacion de DaoDefensas0

--- CapaDatos/View/DaoDefensas.cs
+++ CapaDatos/View/DaoDefensas.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace CapaDatos.View
 {
+    /// <summary>
+    /// clase creada para la obtener las consultas de defensas exitentes
+    /// </summary>
     public class DaoDefensas
     {
         string carrera;
@@ -14,6 +17,9 @@ namespace CapaDatos.View
         string estudiante;
         string key;
 
+        /// <summary>
+        /// se crea el constructor de la clase
+        /// </summary>
         public DaoDefensas()
         {
             carrera = null;
@@ -25,9 +31,14 @@ namespace CapaDatos.View
 
         }
 
+        /// <summary>
+        /// Generamos la clase para el almacenamiento de consultas
+        /// </summary>
         Dictionary<string, string> defensasTipos = new Dictionary<string, string>();
        
-
+        /// <summary>
+        /// Instanciamos la Conexion
+        /// </summary>
         Conexion obj = new Conexion();
 
         public string Carrera { get => carrera; set => carrera = value; }
@@ -35,6 +46,9 @@ namespace CapaDatos.View
         public string Estudiante { get => estudiante; set => estudiante = value; }
         public string Key { get => key; set => key = value; }
 
+        /// <summary>
+        /// Agregamos diferentes tipos de consultas con los requerimientos del usuario
+        /// </summary>
         public void fillDic()
         {
             defensasTipos.Clear();
@@ -54,45 +68,89 @@ namespace CapaDatos.View
 
             defensasTipos.Add("TIPO_ESTUDIANTE", $" SELECT * FROM ViewDefensas WHERE Tipo LIKE '%{this.Tipo}%' AND Estudiante LIKE '%{this.Estudiante}%'   ; ");
         }
+
+        /// <summary>
+        /// filtramos la vista viewDefensas con los requerimientos del usuario
+        /// </summary>
+        /// <param name="carrera"></param>
+        /// <returns></returns>
         public DataTable getDenfensasCarrera(string carrera)
         {
             string sql = $" SELECT * FROM ViewDefensas WHERE Carrera LIKE '%{carrera}%'  ; ";        
             return obj.SelectConexion(sql);
         }
+        /// <summary>
+        /// filtramos viewDefensas entre carrera y tipo elegido por el usuario
+        /// </summary>
+        /// <param name="carrera"></param>
+        /// <param name="tipo"></param>
+        /// <returns></returns>
         public DataTable getDenfensasCarreraTipo(string carrera, string tipo)
         {
             string sql = $" SELECT * FROM ViewDefensas WHERE Carrera LIKE '%{carrera}%' AND Tipo LIKE '%{tipo}%'  ; ";
             return obj.SelectConexion(sql);
         }
+        /// <summary>
+        /// filtramos viewDefensas entre carrera y estudiante elegido por el usuario
+        /// </summary>
+        /// <param name="carrera"></param>
+        /// <param name="estudiante"></param>
+        /// <returns></returns>
         public DataTable getDenfensasCarreraEstudiante(string carrera, string estudiante)
         {
             string sql = $" SELECT * FROM ViewDefensas WHERE Carrera LIKE '%{carrera}%' AND Estudiante LIKE '%{estudiante}%'  ; ";
             return obj.SelectConexion(sql);
         }
-
+        /// <summary>
+        /// filtramos viewDefensas entre carrera, tipo y estudiante elegido por el usuario
+        /// </summary>
+        /// <param name="carrera"></param>
+        /// <param name="tipo"></param>
+        /// <param name="estudiante"></param>
+        /// <returns></returns>
         public DataTable getDenfensasCarreraTipoEstudiante(string carrera, string tipo,string estudiante)
         {
             string sql = $" SELECT * FROM ViewDefensas WHERE Carrera LIKE '%{carrera}%' AND Tipo LIKE '%{tipo}%' AND Estudiante LIKE '%{estudiante}%'   ; ";
             return obj.SelectConexion(sql);
         }
         /********/
+
+        /// <summary>
+        /// obtenemos todas las defensas
+        /// </summary>
+        /// <param name="tipo"></param>
+        /// <returns></returns>
         public DataTable getDenfensas(string tipo = "DEFENSAS")
         {
             fillDic();
             string sql = defensasTipos[tipo];
             return obj.SelectConexion(sql);
-        }
+        }/// <summary>
+         /// filtramos viewDefensas entre tipo elegido por el usuario
+         /// </summary>
+         /// <param name="tipo"></param>
+         /// <returns></returns>
         public DataTable getDenfensasTipo( string tipo)
         {
             string sql = $" SELECT * FROM ViewDefensas WHERE Tipo LIKE '%{tipo}%'  ; ";
             return obj.SelectConexion(sql);
         }
+        /// <summary>
+        /// filtramos viewDefensas entre estudiante elegido por el usuario
+        /// </summary>
+        /// <param name="estudiante"></param>
+        /// <returns></returns>
         public DataTable getDenfensaEstudiante(string estudiante)
         {
             string sql = $" SELECT * FROM ViewDefensas WHERE Estudiante LIKE '%{estudiante}%'  ; ";
             return obj.SelectConexion(sql);
         }
-
+        /// <summary>
+        /// filtramos viewDefensas entre tipo y estudiante elegido por el usuario
+        /// </summary>
+        /// <param name="tipo"></param>
+        /// <param name="estudiante"></param>
+        /// <returns></returns>
         public DataTable getDenfensasTipoEstudiante(string tipo, string estudiante)
         {
             string sql = $" SELECT * FROM ViewDefensas WHERE Tipo LIKE '%{tipo}%' AND Estudiante LIKE '%{estudiante}%'   ; ";
